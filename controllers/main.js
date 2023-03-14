3b7d4d2cbcc09d7cb2821bc1c50667a1dc9e2a16
mazenfteha <mazn34903@gmail.com>
2023-03-14 16:46:31 -0700

    setup authentication so only the request with JWT can access the dashboard

--- controllers/main.js
+++ controllers/main.js
@@ -12,7 +12,7 @@ const login =async (req,res)=>{
     //check in the controller
     const {username,password} =req.body
     if(!username || !password){
-        throw new CustomAPIError('please provide email and password')
+        throw new CustomAPIError('please provide email and password',400)
     }
 
     //create demo id to send into payload
@@ -24,9 +24,23 @@ const login =async (req,res)=>{
 }
 
 const dashboard = async(req,res)=>{
-    const luckNumber =Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello, Mazen`,secret:`Here is your authorized data, you r lucky number
-    is ${luckNumber}`})
+    //setup authentication so only the request with JWT can access the dashboard
+    const authHeader =req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new CustomAPIError('No token provided',401)
+    }
+
+    const token =authHeader.split(' ')[1]
+    //verify whether the token is actually valid
+    try {
+        const decoded =jwt.verify(token,process.env.JWT_SECRET)
+        const luckNumber =Math.floor(Math.random()*100)
+        res.status(200).json({msg:`Hello, ${decoded.username}`,secret:`Here is your authorized data, you r lucky number
+        is ${luckNumber}`})
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this data',401)
+    }
 }
 
 module.exports ={
