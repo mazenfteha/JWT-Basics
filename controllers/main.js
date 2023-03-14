a6545d09cb56709b0d86b3cc5319380ce4e0c9fa
mazenfteha <mazn34903@gmail.com>
2023-03-14 13:42:55 -0700

    check user name &password in post request and create token if exist

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,26 @@
+// *logic:*
+//check username, password in post(login) request
+//if exist create new JWT
+//send back to front-end
+//setup authentication so only the request with JWT can access the dashboard
+
+const jwt =require('jsonwebtoken')
+const CustomAPIError =require('../errors/custom-error')
 
 const login =async (req,res)=>{
-    res.send('Fake Login/Register/Signup Route')
+    //check username, password in post(login) request
+    //check in the controller
+    const {username,password} =req.body
+    if(!username || !password){
+        throw new CustomAPIError('please provide email and password')
+    }
+
+    //create demo id to send into payload
+    const id =new Date().getDate()
+
+    //if exist create new JWT
+    const token =jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'})
+    res.status(200).json({msg:'user created',token})
 }
 
 const dashboard = async(req,res)=>{
